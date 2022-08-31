43cb47b4d4d025e0082924c784b9012e1dbc72f2
TheFluffyNebula <yinj@guilford.edu>
2022-08-30 21:15:25 -0400

    input changes what's under "Six"

--- testscript.js
+++ testscript.js
@@ -3,11 +3,13 @@ thebutton = document.getElementById("thebutton");
 thebutton2 = document.getElementById("thebutton2");
 otheritem = document.getElementById("buttontest");
 otheritem2 = document.getElementById("buttontest2");
+textentry = document.getElementById("myinput");  
 
 myitem.addEventListener("click", onClick); //sets up a listener where clicking myitem will trigger the function onClick
 myitem.addEventListener("mouseout",normalMyItem);
 thebutton.addEventListener("click", onButtonClick);
 thebutton2.addEventListener("click",onButtonClick2);
+textentry.addEventListener("change", onChange);
 
 function onClick() {  //changes myitem's text to lightgreen
     myitem.style.color = "lightgreen";  
@@ -26,4 +28,9 @@ function onButtonClick(){
 function onButtonClick2(){
     otheritem2.style.color = "lightgreen";
     otheritem2.fontSize = "0.8em"
+}
+
+function onChange() {  
+    newtext = myinput.value;  
+    otheritem.innerHTML = newtext;  
 }
