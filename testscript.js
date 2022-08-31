0122026ed2778ae9e70ceaf15c6dc586e4907dbb
TheFluffyNebula <yinj@guilford.edu>
2022-08-30 20:07:08 -0400

    btn => red text

--- testscript.js
+++ testscript.js
@@ -1,7 +1,10 @@
 myitem = document.getElementById("firsttest"); //stores the element with id "firsttest" in document as myitem
+thebutton = document.getElementById("thebutton");  
+otheritem = document.getElementById("buttontest");
 
 myitem.addEventListener("click", onClick); //sets up a listener where clicking myitem will trigger the function onClick
 myitem.addEventListener("mouseout",normalMyItem);
+thebutton.addEventListener("click", onButtonClick);
 
 function onClick() {  //changes myitem's text to lightgreen
     myitem.style.color = "lightgreen";  
@@ -11,4 +14,8 @@ function onClick() {  //changes myitem's text to lightgreen
 function normalMyItem(){
     myitem.style.color = "gold";
     myitem.style.fontSize = "1em";
+}
+
+function onButtonClick(){
+    otheritem.style.color = "red";  
 }
